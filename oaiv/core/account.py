d981e72900dee32e2f50a912e1a5a0760fd1b6cc
edazizovv <edazizovv@gmail.com>
2023-01-11 19:12:31 +0300

    BTC fix: changed main BTC dependent package, fixed segwit support

--- oaiv/core/account.py
+++ oaiv/core/account.py
@@ -1,4 +1,5 @@
 #
+import time
 import json
 import datetime
 
@@ -9,6 +10,12 @@ from web3 import Web3
 from urllib import parse, request
 from cryptos import Bitcoin, random_key
 from eth_account.messages import encode_defunct
+from bitcoinlib.encoding import EncodingError
+from bitcoinlib.config.config import DEFAULT_NETWORK
+from bitcoinlib.services.services import Service
+from bitcoinlib.keys import HDKey, Address, BKeyError
+from oaiv_btc.func import Transactor
+
 
 #
 from oaiv.tools.utils import format_provider, format_w3, data_constructor, check_precision
@@ -83,6 +90,197 @@ class InteractionFunctionality:
 
 
 class InteractionFunctionalityBitcoin:
+    def __init__(self):
+        self.network = DEFAULT_NETWORK
+        self.service = Service(network=self.network, providers=None, cache_uri=None)
+
+    def is_address(self, address):
+        if isinstance(address, str):
+            if len(address) > 0:
+                try:
+                    address = Address.parse(address)
+                    # only p2pkh / p2wpkh addressed derived from compressed public_key are supported
+                    # all other formats (including non-compressed p2pkh / p2sh / p2sh-p2wpkh / p2tr) won't be valid here
+                    return any([(address.encoding == 'base58') and (address.script_type == 'p2pkh'),
+                                (address.encoding == 'bech32') and (address.script_type == 'p2wpkh')])
+                except EncodingError as e:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+    def is_key_pair(self, private_key, address):
+        try:
+            # only p2pkh / p2wpkh addressed derived from compressed public_key are supported
+            # all other formats (including non-compressed p2pkh / p2sh / p2sh-p2wpkh / p2tr)
+            # might have unexpected behavior here
+            address_parsed = Address.parse(address)
+            witness_type = address_parsed.witness_type
+            encoding = address_parsed.encoding
+            script_type = address_parsed.script_type
+            hdkey = HDKey(private_key, compressed=True, encoding=encoding, witness_type=witness_type)
+            address_derived = hdkey.address(script_type=script_type, encoding=encoding)
+            return address_parsed.address == address_derived
+        except BKeyError as e:
+            return False
+        except EncodingError as e:
+            return False
+
+    def balance(self, addresses):
+        result = {}
+        for address in addresses:
+            balance = self.service._provider_execute('getbalance', [address])
+            balance = balance / 100_000_000
+            result[address] = {'BTC': balance}
+        return result
+
+    def get_transactions(self, account, sort='desc', raw=True):
+
+        last_txid = ''
+        max_utxos = 100
+        request_results = self.service._provider_execute('gettransactions', account, last_txid,  max_utxos)
+
+        if not raw:
+            results = {'tx': [], 'datetime': [], 'sender': [], 'receiver': [], 'value': [], 'commission_paid': [],
+                      'currency': []}
+            for tx in request_results:
+
+                self_inputs = [x for x in tx.inputs if x.address == account]
+                self_outputs = [x for x in tx.outputs if x.address == account]
+                if len(self_inputs) > 0:
+                    value = sum([x.value for x in self_inputs]) - sum([x.value for x in self_outputs])
+                    inputs = account
+                    outputs = ';'.join([x.address for x in tx.outputs if x.address != account])
+                else:
+                    value = sum([x.value for x in self_outputs])
+                    inputs = ';'.join([x.address for x in tx.inputs])
+                    outputs = account
+                tx_hash = tx.txid
+                tx_datetime = tx.date
+                commission = tx.fee
+                currency = 'BTC'
+
+                results['tx'].append(tx_hash)
+                results['datetime'].append(tx_datetime)
+                results['sender'].append(inputs)
+                results['receiver'].append(outputs)
+                results['value'].append(value)
+                results['commission_paid'].append(commission)
+                results['currency'].append(currency)
+
+            results = pandas.DataFrame(data=results)
+            results['value'] = results['value'] / 100_000_000
+            results['commission_paid'] = results['commission_paid'] / 100_000_000
+            results = results.sort_values(by='datetime', ascending=(not (sort == 'desc')))
+            results = results.to_dict()
+            re = (results, {})
+        else:
+            re = (request_results, {})
+        return re
+
+    def create_account(self):
+        hdkey = HDKey()
+        private_key = hdkey.private_hex
+        # by default, we generate native segwit only (p2wpkh addresses)
+        actor = Actor(blockchain=BlockchainType.BITCOIN, private_key=private_key, script_type='p2wpkh')
+        return actor
+
+    def make_transaction(self, sender, receiver, value=None, gas=None, **kwargs):
+
+        value = '{0} BTC'.format(value)
+
+        private_hex = sender.private_key
+        from_address = Address.parse(sender.address)
+        witness_type = from_address.witness_type
+        encoding = from_address.encoding
+        script_type = from_address.script_type
+        hdkey = HDKey(private_hex, compressed=True, encoding=encoding, witness_type=witness_type)
+        _ = hdkey.address(script_type=script_type, encoding=encoding)
+        kk = Transactor(address=hdkey.address(), hdkey=hdkey)
+        address_to = Address.parse(receiver.address)
+
+        """
+        try:
+            if gas:
+                tx = kk.send_to(address_to, value, offline=False, fee=gas)
+            else:
+                tx = kk.send_to(address_to, value, offline=False)
+        except Exception as e:
+            print("EXCEPTION CAUGHT")
+            print(e)
+            print("EXCEPTION CAUGHT, SLEEPING FOR 5 MINUTES")
+            time.sleep(300)
+            if gas:
+                tx = kk.send_to(address_to, value, offline=False, fee=gas)
+            else:
+                tx = kk.send_to(address_to, value, offline=False)
+        """
+        if gas:
+            tx = kk.send_to(address_to, value, offline=False, fee=gas)
+        else:
+            tx = kk.send_to(address_to, value, offline=False)
+
+        """
+        if tx.error is not None:
+            time.sleep(1)
+            tx_check = self.service.gettransaction(tx.txid)
+            if not tx_check:
+                time.sleep(5)
+                tx_check = self.service.gettransaction(tx.txid)
+                if not tx_check:
+                    print(tx.info())
+                    print("BAD TRANSACTION, SLEEPING FOR 5 MINUTES")
+                    time.sleep(300)
+                    tx_check = self.service.gettransaction(tx.txid)
+                    if not tx_check:
+                        if gas:
+                            tx = kk.send_to(address_to, value, offline=False, fee=gas)
+                        else:
+                            tx = kk.send_to(address_to, value, offline=False)
+                    time.sleep(10)
+                    tx_check = self.service.gettransaction(tx.txid)
+                    if not tx_check:
+                        raise Exception("Unexpected error. Transaction {0} is not sent to the blockchain\n"
+                                        "Error message:\n{1}".format(tx.txid, tx.error))
+                else:
+                    print(tx.info())
+                    print("Transaction is sent to the blockchain, however an unexpected response received from the providers\n"
+                          "Response message:\n{0}".format(tx.error))
+            else:
+                print(tx.info())
+                print("Transaction is sent to the blockchain, however an unexpected response received from the providers\n"
+                      "Response message:\n{0}".format(tx.error))
+        tx_id = tx.txid
+        """
+
+        if tx.error is not None:
+            time.sleep(1)
+            tx_check = self.service.gettransaction(tx.txid)
+            if not tx_check:
+                time.sleep(5)
+                tx_check = self.service.gettransaction(tx.txid)
+                if not tx_check:
+                    if not tx_check:
+                        print(tx.info())
+                        raise Exception("Unexpected error. Transaction {0} is not sent to the blockchain\n"
+                                        "Error message:\n{1}".format(tx.txid, tx.error))
+                else:
+                    print(tx.info())
+                    print(
+                        "Transaction is sent to the blockchain, however an unexpected response received from the providers\n"
+                        "Response message:\n{0}".format(tx.error))
+            else:
+                print(tx.info())
+                print(
+                    "Transaction is sent to the blockchain, however an unexpected response received from the providers\n"
+                    "Response message:\n{0}".format(tx.error))
+        tx_id = tx.txid
+
+        return tx_id
+
+'''
+class OLDInteractionFunctionalityBitcoin:
     def __init__(self):
         self.utility = Bitcoin(testnet=False)
 
@@ -175,7 +373,7 @@ class InteractionFunctionalityBitcoin:
         tx_id = result['data']['txid']
 
         return tx_id
-
+'''
 
 class InteractionFunctionalityEthereum:
     def __init__(self, etherscan_api_key, ethplorer_api_key, ethereum_network, infura_project_id):
@@ -409,9 +607,90 @@ class Actor:
             return super().__getattribute__('_actor').__getattribute__(item)
 
 
+
+# TODO: change the behavior so that the address is not autogenerated with the private key;
+#  put the current autogeneration to a separate method like 'get_address'    # ??? should it be so ???
+class ActorBitcoin:
+    def __init__(self, private_key=None, address=None, encryption=None, script_type='p2wpkh', **kwargs):
+
+        if private_key and address:
+            if self.is_key_pair(private_key=private_key, address=address):
+                self.private_key = private_key
+                self._address = address
+            else:
+                raise ValueError(
+                    "Invalid address {0} or private key ***** provided; both should match each other".format(address))
+        elif private_key:
+            self.private_key = private_key
+            # we support only (compressed; p2pkh; base58) and (compressed; p2wpkh; bech32) addresses
+            hdkey = HDKey(private_key)
+            if script_type == 'p2pkh':
+                self._address = hdkey.address(script_type=script_type, encoding='base58')
+            elif script_type == 'p2wpkh':
+                self._address = hdkey.address(script_type=script_type, encoding='bech32')
+            else:
+                raise ValueError(
+                    "Invalid script_type value {0} provided; check available script types first".format(
+                        script_type))
+        elif address:
+            if self.is_address(address):
+                self._address = address
+            else:
+                raise ValueError("Invalid address {0} provided; should be a valid Bitcoin address".format(address))
+
+        self.encryption = encryption
+
+    def is_address(self, address):
+        if isinstance(address, str):
+            if len(address) > 0:
+                try:
+                    address = Address.parse(address)
+                    # only p2pkh / p2wpkh addressed derived from compressed public_key are supported
+                    # all other formats (including non-compressed p2pkh / p2sh / p2sh-p2wpkh / p2tr) won't be valid here
+                    return any([(address.encoding == 'base58') and (address.script_type == 'p2pkh'),
+                                (address.encoding == 'bech32') and (address.script_type == 'p2wpkh')])
+                except EncodingError as e:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+    def is_key_pair(self, private_key, address):
+        try:
+            # only p2pkh / p2wpkh addressed derived from compressed public_key are supported
+            # all other formats (including non-compressed p2pkh / p2sh / p2sh-p2wpkh / p2tr)
+            # might have unexpected behavior here
+            address_parsed = Address.parse(address)
+            witness_type = address_parsed.witness_type
+            encoding = address_parsed.encoding
+            script_type = address_parsed.script_type
+            hdkey = HDKey(private_key, compressed=True, encoding=encoding, witness_type=witness_type)
+            address_derived = hdkey.address(script_type=script_type, encoding=encoding)
+            return address_parsed.address == address_derived
+        except BKeyError as e:
+            return False
+        except EncodingError as e:
+            return False
+
+    @property
+    def address(self):
+        return self._address
+
+    @address.setter
+    def address(self, value):
+        if self.is_address(value):
+            self._address = value
+        else:
+            raise ValueError("Invalid address {0} provided; should be a valid Bitcoin address".format(value))
+
+    def sign_transaction(self, tx):
+        raise NotImplementedError("In the current version of oaiv transactions are signed on make_transaction level")
+
+'''
 # TODO: change the behavior so that the address is not autogenerated with the private key;
 #  put the current autogeneration to a separate method like 'get_address'
-class ActorBitcoin:
+class OLDActorBitcoin:
     def __init__(self, private_key=None, address=None, encryption=None, address_type='p2pkh', **kwargs):
         self._b = Bitcoin(testnet=False)
         self.private_key = private_key
@@ -457,9 +736,10 @@ class ActorBitcoin:
             return self._b.signall(tx, self.private_key)
         else:
             raise Exception("You have to provide a private_key to use this feature")
-
+'''
 
 # TODO: add mnemonic support (see the w3.eth.account docs)
+# TODO: add encoding support
 # TODO: add importing & exporting features
 class ActorEthereum:
     def __init__(self, w3, private_key=None, address=None, encryption=None, **kwargs):
